38e40ebf7a3a25ccb870e445bb821adc7e9e97b6
oddlyspaced <oddlyspaced1@gmail.com>
2024-09-13 19:56:07 +0530

    push

---
+++ src/main/kotlin/medium/Problem1310.kt
@@ -0,0 +1,26 @@
+package medium
+
+class Problem1310 {
+    fun xorQueries(arr: IntArray, queries: Array<IntArray>): IntArray {
+        val res = IntArray(queries.size)
+        queries.forEachIndexed { index, query ->
+            for (i in query[0]..query[1]) {
+                res[index] = res[index] xor arr[i]
+            }
+        }
+        return res
+    }
+}
+
+fun main() {
+    println(
+        Problem1310().xorQueries(
+            intArrayOf(1, 3, 4, 8), arrayOf(
+                intArrayOf(0, 1),
+                intArrayOf(1, 2),
+                intArrayOf(0, 3),
+                intArrayOf(3, 3)
+            )
+        ).joinToString(",")
+    )
+}
