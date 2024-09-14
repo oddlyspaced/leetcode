b1855c64f331ddeec64bf3100cbd11352535690e
oddlyspaced <oddlyspaced1@gmail.com>
2024-09-14 23:59:42 +0530

    push

--- src/main/kotlin/medium/Problem46.kt
+++ src/main/kotlin/medium/Problem46.kt
@@ -2,26 +2,14 @@
 //
 //class Problem46 {
 //    fun permute(nums: IntArray): List<List<Int>> {
-//        gen(nums, arrayListOf())
-//        return data
-//    }
+//        val res = arrayListOf<List<Int>>()
+//         fun combine(index: Int, res: ArrayList<Int>) {
 //
-//    private fun gen(nums: IntArray, temp: ArrayList<Int>, data: ArrayList<List<Int>>) {
-//        if (temp.size == nums.size) {
-//            data.add(temp)
-//        } else {
-//            nums.forEach {
-//                if (!temp.contains(it)) {
-//                    temp.add(it)
-//                    gen(nums, temp, data)
-//                }
-//            }
-//        }
+//         }
 //    }
 //}
 //
 //fun main() {
-////    Problem46().gen(intArrayOf(1, 2, 3), hashSetOf())
 //    Problem46().permute(intArrayOf(1, 2, 3)).forEach {
 //        println(it.joinToString(","))
 //    }
