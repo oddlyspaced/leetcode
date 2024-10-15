06e25ebaaf708a4059d4bf9b73ad5a868846f0d2
oddlyspaced <oddlyspaced1@gmail.com>
2024-10-15 06:48:08 +0530

    push

---
+++ src/main/kotlin/medium/Problem2938.kt
@@ -0,0 +1,21 @@
+package medium
+
+class Problem2938 {
+    fun minimumSteps(s: String): Long {
+        var zero = 0
+        var steps = 0L
+        for (i in s.indices) {
+            if (s[i] == '0') {
+                steps += i - zero
+                zero += 1
+            }
+        }
+        return steps
+    }
+}
+
+fun main() {
+    println(Problem2938().minimumSteps("101"))
+    println(Problem2938().minimumSteps("100"))
+    println(Problem2938().minimumSteps("11000111"))
+}
