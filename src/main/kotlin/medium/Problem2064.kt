6ed0de9441b7d9629c1ac0d5e880b1fba6453c53
oddlyspaced <oddlyspaced1@gmail.com>
2024-11-14 22:14:08 +0530

    push

---
+++ src/main/kotlin/medium/Problem2064.kt
@@ -0,0 +1,32 @@
+package medium
+
+import kotlin.math.ceil
+
+class Problem2064 {
+    fun minimizedMaximum(n: Int, quantities: IntArray): Int {
+        fun checkDist(x: Int): Boolean {
+            val s = quantities.sumOf {
+                ceil(it / x.toDouble())
+            }
+            return s.toInt() <= n
+        }
+
+        var l = 1
+        var r = quantities.max()
+        var res = 0
+        while (l <= r) {
+            val mid = (l + r) / 2
+            if (checkDist(mid)) {
+                res = mid
+                r = mid - 1
+            } else {
+                l = mid + 1
+            }
+        }
+        return res
+    }
+}
+
+fun main() {
+    println(Problem2064().minimizedMaximum(6, intArrayOf(11, 6)))
+}
