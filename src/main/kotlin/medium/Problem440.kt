1603370a8c5023d82a386e024eb54a5596e0fb42
oddlyspaced <oddlyspaced1@gmail.com>
2024-11-24 21:10:09 +0530

    push

---
+++ src/main/kotlin/medium/Problem440.kt
@@ -0,0 +1,23 @@
+package medium
+
+import java.util.*
+
+class Problem440 {
+
+    val result = ArrayList<Int>()
+
+    private fun dfs(number: Int, n: Int) {
+        if (number > n) return
+        result.add(number)
+        for (i in 0..9) {
+            dfs(number * 10 + i, n)
+        }
+    }
+
+    fun findKthNumber(n: Int, k: Int): Int {
+        for (i in 1..9) {
+            dfs(i, n)
+        }
+        return result[k]
+    }
+}
