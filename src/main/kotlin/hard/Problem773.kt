20b92574b13863625a8ba5763e89bf2bc3f893f4
oddlyspaced <oddlyspaced1@gmail.com>
2024-12-14 16:18:09 +0530

    push

---
+++ src/main/kotlin/hard/Problem773.kt
@@ -0,0 +1,11 @@
+package hard
+
+class Problem773 {
+    fun slidingPuzzle(board: Array<IntArray>): Int {
+        val adj
+    }
+}
+
+fun main() {
+    println(Problem773().slidingPuzzle(arrayOf(intArrayOf(1, 2, 3), intArrayOf(4, 0, 5))))
+}
