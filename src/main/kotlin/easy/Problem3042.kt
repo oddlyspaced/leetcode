f26dd1db15d73217cf7570eb086ef66a627ad252
oddlyspaced <oddlyspaced1@gmail.com>
2025-01-08 09:36:55 +0530

    daily problem jan 8

---
+++ src/main/kotlin/easy/Problem3042.kt
@@ -0,0 +1,19 @@
+package easy
+
+class Problem3042 {
+    fun countPrefixSuffixPairs(words: Array<String>): Int {
+        var res = 0
+        for (i in words.indices) {
+            for (j in (i + 1)..<words.size) {
+                if (words[j].startsWith(words[i]) && words[j].endsWith(words[i])) {
+                    res++
+                }
+            }
+        }
+        return res
+    }
+}
+
+fun main() {
+    println(Problem3042().countPrefixSuffixPairs(arrayOf("a", "aba", "ababa", "aa")))
+}
