e4d39b62b3e7fa0a62a4af021aeb91376c566df5
oddlyspaced <oddlyspaced1@gmail.com>
2025-03-13 23:23:15 +0530

    problem 3355

---
+++ src/main/kotlin/medium/Problem3355.kt
@@ -0,0 +1,20 @@
+package medium
+
+class Problem3355 {
+    fun isZeroArray(nums: IntArray, queries: Array<IntArray>): Boolean {
+        val line = IntArray(nums.size + 1)
+        queries.forEach {
+            line[it[0]]++
+            line[it[1] + 1]--
+        }
+        for (i in 1..<line.size) {
+            line[i] += line[i - 1]
+        }
+        for (i in nums.indices) {
+            if (line[i] < nums[i]) {
+                return false
+            }
+        }
+        return true
+    }
+}
