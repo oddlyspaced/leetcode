e3bb8435ad3c946713abab5f074230f3498a5f5c
oddlyspaced <oddlyspaced1@gmail.com>
2025-03-31 14:58:57 +0530

    problem 3289

---
+++ src/main/kotlin/easy/Problem3289.kt
@@ -0,0 +1,21 @@
+package easy
+
+/**
+ * Created by Hardik Srivastava on 31/03/25
+ */
+class Problem3289 {
+    fun getSneakyNumbers(nums: IntArray): IntArray {
+        val count = hashMapOf<Int, Int>()
+        nums.forEach {
+            count[it] = count.getOrDefault(it, 0) + 1
+        }
+        val res = IntArray(2)
+        var index = 0
+        count.forEach {
+            if (it.value == 2) {
+                res[index++] = it.key
+            }
+        }
+        return res
+    }
+}
